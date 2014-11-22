1db7727b4054f4b49d8290b7fe89612410b06bbe
Jaakko Tepponen <jaakko.tepponen@rovio.com>
2014-11-22 21:21:34 +0200

    mobile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,64 @@
 /* global process */
 module.exports = function(grunt) {
-    'use strict';
+  'use strict';
 
-    require('time-grunt')(grunt);
-    require('jit-grunt')(grunt);
+  require('time-grunt')(grunt);
+  require('jit-grunt')(grunt);
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        watch: {
-          scripts: {
-            files: ['components/**/*'],
-            tasks: ['quickbuild'],
-          }
-        },
+    watch: {
+      scripts: {
+        files: [
+          'components/**/*',
+          'assets/css/src/*.css'
+        ],
+        tasks: ['quickbuild'],
+      }
+    },
 
-        react: {
-            jsx: {
-              files: {
-                'app_all.js': [
-                  'components/todo/TodoList.js',
-                  'components/todo/TodoItem.js',
-                  'components/todo/TodoInput.js',
-                  'components/todo/TodoClear.js',
-                  'components/todo/Todo.js'
-                ]
-              }
-            }
-          }
-    });
+    react: {
+      jsx: {
+        files: {
+          'app_all.js': [
+          'components/todo/TodoList.js',
+          'components/todo/TodoItem.js',
+          'components/todo/TodoInput.js',
+          'components/todo/TodoClear.js',
+          'components/todo/Todo.js'
+          ]
+        }
+      }
+    },
 
-    grunt.registerTask('default', [], function() {
-        grunt.task.run(
-            'quickbuild'
-        );
-    });
+    autoprefixer: {
+      prefix: {
+        expand: true,
+        flatten: true,
+        src: 'assets/css/src/*.css',
+        dest: 'assets/css/dist/'
+      }
+    }
+  });
 
-    grunt.registerTask('dev', [], function() {
-        grunt.task.run([
-            'quickbuild',
-            'watch',
-        ]);
-    });
+  grunt.registerTask('default', [], function() {
+    grunt.task.run(
+      'quickbuild'
+      );
+  });
 
-    grunt.registerTask('quickbuild', [], function() {
-        grunt.task.run(
-            'react'
-        );
-    });
+  grunt.registerTask('dev', [], function() {
+    grunt.task.run([
+      'quickbuild',
+      'watch',
+      ]);
+  });
+
+  grunt.registerTask('quickbuild', [], function() {
+    grunt.task.run(
+      'react',
+      'autoprefixer'
+      );
+  });
 };
